ec06b9bea64f8d63b333a2db081755d9beb6f4ff
SyedaKhadijaMazhar <khadijamazhar1995@gmail.com>
2021-04-26 14:09:16 +0800

    first commit

---
+++ DatesAndTimes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatesAndTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          //  DateTime myValue = DateTime.Now;
+            //Console.WriteLine(myValue);
+            // Console.WriteLine(myValue.ToShortDateString());
+            //  Console.WriteLine(myValue.ToLongDateString());
+            //Console.WriteLine(myValue.ToShortTimeString());
+            //  Console.WriteLine(myValue.ToLongTimeString());
+            // Console.WriteLine(myValue.AddDays(3).ToLongDateString());
+            // Console.WriteLine(myValue.AddHours(3).ToLongTimeString());
+            //   Console.WriteLine(myValue.AddDays(-3).ToLongDateString());
+            //Console.WriteLine(myValue.Month);
+            // DateTime myBday = new DateTime(1995, 4, 2);
+            //Console.WriteLine(myBday.ToShortDateString());
+            //   Console.WriteLine(myBday.ToLongDateString());
+            DateTime myBday =  DateTime.Parse("1995/4/2");
+            TimeSpan myAge = DateTime.Now.Subtract(myBday);
+            Console.WriteLine(myAge.TotalDays);
+
+            Console.ReadLine();
+        }
+    }
+}
